19a2fe98c2f6f9b8713fe0d43053f408fa654e51
Tyler Woods <tyjordan.woods@gmail.com>
2020-06-02 14:53:47 -0700

    modify train_model2.py

--- train_model2.py
+++ train_model2.py
@@ -8,14 +8,14 @@ python train_mask_detector.py --dataset dataset
 
 # import the necessary packages
 import numpy as np
-# from tensorflow.keras.preprocessing.image import ImageDataGenerator
-# from tensorflow.keras.applications import MobileNetV2
-# from tensorflow.keras.models import Model
-# from tensorflow.keras.layers import AveragePooling2D, Dropout, Flatten, Dense, Input
-# from tensorflow.keras.optimizers import Adam
-# from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-# from tensorflow.keras.preprocessing.image import img_to_array, load_img
-# from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import AveragePooling2D, Dropout, Flatten, Dense, Input
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
